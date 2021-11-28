7952d24611272c23507e407e70b3ef4f8531c19e
BOJIT <35117353+BOJIT@users.noreply.github.com>
2021-11-28 10:13:05 +0000

    simplify: use inbuilt error code set

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,4 @@
 # Set token environment variable
 export PLATFORMIO_AUTH_TOKEN=$1
 
-retVal=$(pio package publish)
-if [ $retVal -ne 0 ]; then
-    echo "Error - could not deploy to PlatformIO Registry"
-fi
-exit $retVal
+pio package publish
